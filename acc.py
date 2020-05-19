af7d961670e749c356e0b638f2145a6f742cb107
Tiji Mathew <8884797+tijimathew@users.noreply.github.com>
2020-05-19 10:18:54 -0400

    added withdrawal limits and transfer logic

--- acc.py
+++ acc.py
@@ -1,24 +1,35 @@
 """
 A bank account has an initial balance
+cannot with
+
 """
 
 class Account:
 
-    def __init__(self, filepath):
+    def __init__(self, filepath, balance = 1000):
         self.filepath = filepath
-        self.initial_value = 1000
+        self.is_withdraw_success = False
         with open(filepath, 'r') as file:
-            current_value = file.read()
-            if current_value == "":
-                self.balance = self.initial_value
+            current_balance = file.read()
+            if current_balance == "":
+                self.balance = balance
             else: 
-                self.balance = int(current_value)
+                self.balance = int(current_balance)
+
+    def is_funds_available(self, amount):
+        return self.balance >= amount
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.is_funds_available(amount):
+            self.balance -= amount
+            self.commit()
+            return True
+        else:
+            return False
 
     def deposit(self, amount):
         self.balance += amount
+        self.commit()
 
     def commit(self):
         with open(self.filepath, 'w') as file:
@@ -31,10 +42,26 @@ class Checking(Account):
         Account.__init__(self, filepath)
         self.fee = fee
     
-    def transfer(self, amount):
-        self.balance -= amount + self.fee
+    def transfer(self, amount, recipient):
+        if self.withdraw(amount + self.fee):
+            recipient.deposit(amount)
+            return True
+        else:
+            return False
 
-checking = Checking("balance.txt", 1)
-checking.transfer(10)
-checking.commit()
-print(checking.balance)
+print("Current balances are :")
+
+john_checking = Checking("john.txt", 1)
+print("John: %s " % john_checking.balance)
+
+jack_checking = Checking("jack.txt", 1)
+print("Jack: %s " % jack_checking.balance)
+
+if john_checking.transfer(10, jack_checking):
+    print("Transfer was completed. Current balances are :")
+    print("John: %s " % john_checking.balance)
+    print("Jack: %s " % jack_checking.balance)
+else: 
+    print("Transfer was NOT completed. Current balances are :")
+    print("John: %s " % john_checking.balance)
+    print("Jack: %s " % jack_checking.balance)
